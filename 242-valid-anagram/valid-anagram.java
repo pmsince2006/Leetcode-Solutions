05a34dda421a404543b1d75cb90f6e737f8b3290
Prince Mathur <princemathur1596@gmail.com>
2026-07-24 09:45:15 +0530

    Time: 3 ms (93.48%) | Memory: 46.5 MB (34.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,28 +1,47 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+
+        // HashMap<Character,Integer> map = new HashMap<>();
+
+        // for(int i = 0; i < s.length();i++){
+        //     char ch = s.charAt(i);
+        //     map.put(ch,map.getOrDefault(ch,0)+1);
+        // }
+
+        // for(int i = 0; i< t.length(); i++){
+        //     char ch = t.charAt(i);
+        //     if(!map.containsKey(ch)){
+        //         return false;
+        //     }
+        //     map.put(ch,map.get(ch)-1);
+
+        //     if(map.get(ch) < 0){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
         if(s.length() != t.length()){
             return false;
         }
 
-        HashMap<Character,Integer> map = new HashMap<>();
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
 
-        for(int i = 0; i < s.length();i++){
-            char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
+        Arrays.sort(s1);
+        Arrays.sort(s2);
 
-        for(int i = 0; i< t.length(); i++){
-            char ch = t.charAt(i);
-            if(!map.containsKey(ch)){
+        int i = 0;
+        while(i < s1.length && i < s2.length){
+            if(s1[i] != s2[i]){
                 return false;
             }
-            map.put(ch,map.get(ch)-1);
-
-            if(map.get(ch) < 0){
-                return false;
-            }
+            i++;
         }
-
         return true;
     }
 }
