a16406c0beb68aa9a005911cbd92c391a4eed6ff
Prince Mathur <princemathur1596@gmail.com>
2026-08-10 07:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (70.07%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,21 +1,43 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid] < target){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
+    static int binary(int low,int high,int[] nums, int target){
+        if(low > high){
+            return -1;
         }
-        return -1;
+
+        int mid = low + (high - low) / 2;
+
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid] < target){
+            return binary(mid+1,high,nums,target);
+        }
+        else{
+            return binary(low,mid-1,nums,target);
+        }
+    }
+    public int search(int[] nums, int target) {
+        
+        // Iterative Method
+        // int low = 0;
+        // int high = nums.length - 1;
+
+        // while(low <= high){
+        //     int mid = low + (high - low) / 2;
+
+        //     if(nums[mid] == target){
+        //         return mid;
+        //     }
+        //     else if(nums[mid] < target){
+        //         low = mid + 1;
+        //     }
+        //     else{
+        //         high = mid - 1;
+        //     }
+        // }
+        // return -1;
+
+        // Recursive Method
+        return binary(0,nums.length - 1, nums, target);
     }
 }
