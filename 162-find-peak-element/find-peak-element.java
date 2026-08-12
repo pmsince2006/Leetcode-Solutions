24c7b3a2995f93319b364b54a3d5c75255f4ac45
Prince Mathur <princemathur1596@gmail.com>
2026-08-12 06:45:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (94.80%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,18 +1,44 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        // int n = nums.length;
+        // int max = Integer.MIN_VALUE;
+        // int a = 0;
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] > max) {
+        //         max = nums[i];
+        //     }
+        // }
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] == max) {
+        //         a = i;
+        //     }
+        // }
+        // return a;
+
         int n = nums.length;
-        int max =Integer.MIN_VALUE;
-        int a = 0;
-        for(int i = 0;i < n;i++){
-            if(nums[i]>max){
-            max = nums[i];
+        if(n == 1){
+            return 0;
+        }
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+        if(nums[n-1] > nums[n-2]){
+            return n-1;
+        }
+        int low = 1;
+        int high = n-2;
+        while(low <= high){
+            int mid = (low+high)/ 2;
+            if((nums[mid] > nums[mid -1]) && nums[mid] > nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid] > nums[mid - 1]){
+                low =  mid + 1;
+            }
+            else{
+                high = mid - 1;
             }
         }
-        for(int i = 0;i < n;i++){
-            if(nums[i]==max){
-             a=i;
-            }
-        }
-      return a;
-}
+        return -1;
+    }
 }
