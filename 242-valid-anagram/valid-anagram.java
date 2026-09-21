3f9c4bbdae414f95a239366e900f3822b5c6c12d
Prince Mathur <princemathur1596@gmail.com>
2026-09-21 10:53:19 +0530

    Time: 17 ms (16.56%) | Memory: 46.4 MB (33.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,44 +1,44 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // if(s.length() != t.length()){
-        //     return false;
-        // }
-
-        // HashMap<Character,Integer> map = new HashMap<>();
-
-        // for(int i = 0; i < s.length();i++){
-        //     char ch = s.charAt(i);
-        //     map.put(ch,map.getOrDefault(ch,0)+1);
-        // }
-
-        // for(int i = 0; i< t.length(); i++){
-        //     char ch = t.charAt(i);
-        //     if(!map.containsKey(ch)){
-        //         return false;
-        //     }
-        //     map.put(ch,map.get(ch)-1);
-
-        //     if(map.get(ch) < 0){
-        //         return false;
-        //     }
-        // }
-
-        // return true;
-
         if(s.length() != t.length()){
             return false;
         }
-        int[] freq = new int[26];
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
         for(int i = 0; i < s.length();i++){
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        for(int i = 0; i< 26; i++){
-            if(freq[i] != 0){
+
+        for(int i = 0; i< t.length(); i++){
+            char ch = t.charAt(i);
+            if(!map.containsKey(ch)){
+                return false;
+            }
+            map.put(ch,map.get(ch)-1);
+
+            if(map.get(ch) < 0){
                 return false;
             }
         }
+
         return true;
+
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // int[] freq = new int[26];
+        // for(int i = 0; i < s.length();i++){
+        //     freq[s.charAt(i) - 'a']++;
+        //     freq[t.charAt(i) - 'a']--;
+        // }
+        // for(int i = 0; i< 26; i++){
+        //     if(freq[i] != 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
 
         // if(s.length() != t.length()){
         //     return false;
@@ -50,13 +50,13 @@ class Solution {
         // Arrays.sort(s1);
         // Arrays.sort(s2);
 
-        // // int i = 0;
-        // // while(i < s1.length && i < s2.length){
-        // //     if(s1[i] != s2[i]){
-        // //         return false;
-        // //     }
-        // //     i++;
-        // // }
+        // int i = 0;
+        // while(i < s1.length && i < s2.length){
+        //     if(s1[i] != s2[i]){
+        //         return false;
+        //     }
+        //     i++;
+        // }
 
         // return Arrays.equals(s1,s2);
     }
